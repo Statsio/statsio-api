aa4a5aecba249a517baa901b90e5a9061dd62f1e
Corentin GESSE-ENTRESSANGLE <corentin.ckc@gmail.com>
2026-07-03 17:43:25 +0200

    fix google account register

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -32,6 +32,7 @@ class User extends Authenticatable
         'email',
         'password',
         'google_id',
+        'locale',
         'email_verified_at',
         'status',
         'is_admin',
